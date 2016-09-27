9e8f3bac8e223f5e4d84c32003f463d609d6ccc3
Mikey Stewart <mikey.snooker147@gmail.com>
2016-09-27 14:26:16 +1300

    Mikey's update to NPC script
    Script now has methods for collisions between npc and player. Use these
    methods for interactions.

--- Politico_peace_or_war/Assets/_Scripts/NPCMovement.cs
+++ Politico_peace_or_war/Assets/_Scripts/NPCMovement.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class NPCMovement : MonoBehaviour {
 
 	public int walkingSpeed = 150;
-	public float maxDistanceFromStartPoint = 30;
+	public float maxDistanceFromStartPoint = 30; //Use for keeping NPC within a certain area
 	public int timeToWait = 60;
 
-	//private bool toWalk = false;
+	private bool isTalking;
 	private int delayCounter;
 	private float distanceToWalk;
 	private int currentDirection; //0 = up, 1 = right, 2 = down, 3 = left, 4 = idle
-	private float startingX;
-	private float startingY;
+	private float startingX; //Use for keeping NPC within a certain area
+	private float startingY; //Use for keeping NPC within a certain area
 	private Rigidbody2D rigidbody;
 	private Animator anim;
 	private Vector2 movementVector;
@@ -25,32 +25,40 @@ public class NPCMovement : MonoBehaviour {
 		startingY = transform.position.y;
 		distanceToWalk = 100;
 		currentDirection = 2;
+		isTalking = false;
 		movementVector = new Vector2(0, 0);
 
 	}
 
 	void Update () {
-		directionChange ();
-		if (currentDirection == 0) {
-			movementVector = new Vector2(0, 1);
-		} else if (currentDirection == 1) {
-			movementVector = new Vector2(1, 0);
-		} else if (currentDirection == 2) {
-			movementVector = new Vector2(0, -1);
-		} else if (currentDirection == 3) {
-			movementVector = new Vector2(-1, 0);
-		} else if (currentDirection == 4) {
-			movementVector = new Vector2(0, 0);
-		}
-		
-		if (movementVector != Vector2.zero) {
-			anim.SetBool("isWalking", true);
-			anim.SetFloat("input_x", movementVector.x);
-			anim.SetFloat("input_y", movementVector.y);
-		} else {
+		Debug.Log("Test");
+		if(isTalking) {
 			anim.SetBool("isWalking", false);
+			rigidbody.MovePosition (rigidbody.position); //Hopefully keeps the sprite in the same position
+		} else {
+			directionChange ();
+			if (currentDirection == 0) {
+				movementVector = new Vector2(0, 1);
+			} else if (currentDirection == 1) {
+				movementVector = new Vector2(1, 0);
+			} else if (currentDirection == 2) {
+				movementVector = new Vector2(0, -1);
+			} else if (currentDirection == 3) {
+				movementVector = new Vector2(-1, 0);
+			} else if (currentDirection == 4) {
+				movementVector = new Vector2(0, 0);
+			}
+			
+			if (movementVector != Vector2.zero) {
+				anim.SetBool("isWalking", true);
+				anim.SetFloat("input_x", movementVector.x);
+				anim.SetFloat("input_y", movementVector.y);
+			} else {
+				anim.SetBool("isWalking", false);
+			}
+			rigidbody.MovePosition (rigidbody.position + movementVector * Time.smoothDeltaTime * walkingSpeed);
 		}
-		rigidbody.MovePosition (rigidbody.position + movementVector * Time.smoothDeltaTime * walkingSpeed);
+
 	}
 
 	void directionChange () {
@@ -59,6 +67,24 @@ public class NPCMovement : MonoBehaviour {
 			distanceToWalk = Random.Range (0, 50);
 		} else {
 			distanceToWalk--;
+		}
+	}
+
+	//When he collides with another collider
+	void OnCollisionEnter2D(Collision2D col) {
+		Debug.Log ("On collision entered");
+		if (col.gameObject.name == "Player") {
+			Debug.Log("collided with 'player'");
+			isTalking = true;
+		}
+	}
+
+	//When he stops colliding with something
+	void OnCollisionExit2D(Collision2D col) {
+		Debug.Log ("On collision exitted");
+		if (col.gameObject.name == "Player") {
+			Debug.Log("un collided with 'player'");
+			isTalking = false;
 		}
 	}
 
